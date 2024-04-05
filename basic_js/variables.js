0041131d9f6536bd9870ee9e3a2c57e663de849f
Ahammed  Shorif <79597517+ahammedshorif@users.noreply.github.com>
2024-04-05 19:59:49 +0000

    variables

---
+++ basic_js/variables.js
@@ -0,0 +1,24 @@
+/* var- can be change
+ now day not use--> scop problem (so use let) */
+
+var a= 10;
+console.log(a);
+a=20
+console.log(a);
+
+
+// let  ----> No scoping proble
+
+let account; // undefined
+
+
+/* 
+  const
+ const variable can not be change
+ */
+
+const name= "shorif";
+//  name="ahammed"; -----> wrong 
+
+console.table([a,name,account]); // print multiple values in table form
+
